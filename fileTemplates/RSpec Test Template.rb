c783c1418b96f2c4a23c2c2b68b9c98b1c44b7e4
T1D <t1d@asolutions.com>
2011-02-17 11:24:33 -0600

    Add config

---
+++ fileTemplates/RSpec Test Template.rb
@@ -0,0 +1,11 @@
+#parse("Ruby File Header.rb")
+require 'spec_helper'
+
+describe ${Behaviour_Name} do
+
+  it "should ${it_should}" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
